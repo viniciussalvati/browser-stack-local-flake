b930a4caa397198a6ceea03b9abd9ef7e36acb96
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-10-21 16:46:24 +0100

    Refactored the code to allow the derivation to be overridable

--- flake.nix
+++ flake.nix
@@ -7,63 +7,55 @@
 
   outputs =
     { self, nixpkgs }:
+    with nixpkgs.legacyPackages.x86_64-linux;
+    let
+      mkBrowserStackLocal =
+        { url, hash }:
+        stdenvNoCC.mkDerivation {
+          name = "BrowserStackLocal";
+
+          src = fetchurl {
+            inherit url hash;
+          };
+
+          buildInputs = [
+            gcc-unwrapped
+          ];
+
+          nativeBuildInputs = [
+            autoPatchelfHook
+            unzip
+          ];
+
+          sourceRoot = ".";
+
+          unpackPhase = ''
+            unzip $src
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            install -m755 -D BrowserStackLocal $out/bin/BrowserStackLocal
+            runHook postInstall
+          '';
+
+          meta = {
+            homepage = "https://www.browserstack.com/";
+            description = "BrowserStack Local testing app";
+            downloadPage = "https://www.browserstack.com/docs/live/local-testing/set-up-local-testing#Linux";
+            # license = lib.licenses.unfreeRedistributable;
+            platforms = [ "x86_64-linux" ];
+          };
+        };
+    in
     {
-      lib = {
-        mkBrowserStackLocal =
-          {
-            pkgs,
-            srcUrl ? "https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip",
-            srcHash ? "sha256-EloQ1oOT2FeFLMLlGsKv1SJrrNmvy4YNuTKJMgnI0y4=",
-          }:
-          with pkgs;
-          stdenv.mkDerivation (self: {
-            # Both of these options should be overrideable.
-            # Useful in case the hash changes and the flake has not been updated
-            inherit srcUrl srcHash;
+      packages.x86_64-linux = {
+        BrowserStackLocal = nixpkgs.lib.makeOverridable mkBrowserStackLocal {
+          url = "https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip";
+          hash = "sha256-EloQ1oOT2FeFLMLlGsKv1SJrrNmvy4YNuTKJMgnI0y4=";
+        };
 
-            name = "BrowserStackLocal";
-
-            src = fetchurl {
-              url = self.srcUrl;
-              hash = self.srcHash;
-            };
-
-            buildInputs = [
-              gcc-unwrapped
-            ];
-
-            nativeBuildInputs = [
-              autoPatchelfHook
-              unzip
-            ];
-
-            sourceRoot = ".";
-
-            unpackPhase = ''
-              unzip $src
-            '';
-
-            installPhase = ''
-              runHook preInstall
-              install -m755 -D BrowserStackLocal $out/bin/BrowserStackLocal
-              runHook postInstall
-            '';
-
-            meta = {
-              homepage = "https://www.browserstack.com/";
-              description = "BrowserStack Local testing app";
-              downloadPage = "https://www.browserstack.com/docs/live/local-testing/set-up-local-testing#Linux";
-              # license = lib.licenses.unfreeRedistributable;
-              platforms = [ "x86_64-linux" ];
-            };
-          });
+        default = self.packages.x86_64-linux.BrowserStackLocal;
       };
-
-      packages.x86_64-linux.BrowserStackLocal = self.lib.mkBrowserStackLocal {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      };
-
-      packages.x86_64-linux.default = self.packages.x86_64-linux.BrowserStackLocal;
-
     };
 }
