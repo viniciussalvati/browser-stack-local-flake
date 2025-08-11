4e90c1858de3e830e9fde5a381342bb08a6a642f
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2025-08-11 18:53:18 +0100

    Updated the hash

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       packages.x86_64-linux = {
         BrowserStackLocal = nixpkgs.lib.makeOverridable mkBrowserStackLocal {
           url = "https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip";
-          hash = "sha256-eP7xQak1W/S9eQXeTU0vmdqSr4Y6Jqa2tL2qiGpmhQo=";
+          hash = "sha256-x5wu5bNjkQTWyg6Y5uf4tkTBHBebtW9VQbSEskhK9us=";
         };
 
         default = self.packages.x86_64-linux.BrowserStackLocal;
